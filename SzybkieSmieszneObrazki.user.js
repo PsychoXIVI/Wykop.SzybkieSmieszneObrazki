a8dd51a935c09f8e6bab9b7d050a3df977d4a9eb
PsychoX <psychoxivi@gmail.com>
2018-07-28 21:39:16 +0200

    Added a little bit of config (on top of the script)

--- SzybkieSmieszneObrazki.user.js
+++ SzybkieSmieszneObrazki.user.js
@@ -1,21 +1,33 @@
 // ==UserScript==
 // @name        Wykop.SzybkieSmieszneObrazki
+// @namespace   Wykop
 // @description Dodatek dla Wykop.pl pozwalający na szybsze dodawanie obrazków.
 // @author      PsychoX (psychoxivi@gmail.com)
-// @version     1.1
+// @version     1.2
 // @include     *://*wykop.pl/*
-// @grant       none
 // @downloadURL https://raw.githubusercontent.com/PsychoXIVI/Wykop.SzybkieSmieszneObrazki/master/SzybkieSmieszneObrazki.user.js
 // @updateURL   https://raw.githubusercontent.com/PsychoXIVI/Wykop.SzybkieSmieszneObrazki/master/SzybkieSmieszneObrazki.user.js
 // ==/UserScript==
 
 (function (){
-    // Greasemonkey jQuery from current window
+	// Configuration
+	//	Here you can configure the script.
+	config = {
+		'pasteImageFile': 		true,
+		'pasteImageHTML': 		true,
+		'pasteImageLink': 		true,
+		'doNotChangeIfLink': 	true
+	};
+	config.get = function (what) {
+		return this[what];
+	}
+	
+	// Greasemonkey jQuery from current window
     $ = $ || unsafeWindow && unsafeWindow.$;
 	if (!$) {
 		return;
 	}
-	
+    
     $(document).ready(function () {
         /* Consts */
 		const imageURLRegex = /https?:\/\/[^\s]*\/[^\s]*(?:jpe?g|png|gif|bmp|svg)/gi;
@@ -39,17 +51,17 @@
 			// Set the URL of image
 			if (url) {
 				// Open (and hide) add media form 
-				context.find(".openAddMediaOverlay").click();
-				context.find(".addMediaOverlay").css("display", "none");
-				context.find(".overlay").css("display", "none");
+				context.find('.openAddMediaOverlay').click();
+				context.find('.addMediaOverlay').css('display', 'none');
+				context.find('.overlay').css('display', 'none');
 				
 				// Set URL and submit form
-				context.find(".embedUrl")[0].value = url; 
-				context.find(".embedUrl").closest("form").submit();
+				context.find('.embedUrl')[0].value = url; 
+				context.find('.embedUrl').closest('form').submit();
 			}
 			else {
 				// Remove any attachment
-				context.find(".removeAttachemnt").click();
+				context.find('.removeAttachemnt').click();
 			}
 		}
 		
@@ -66,15 +78,15 @@
                 console.log(item);
 				
                 switch (item.kind) {
-					case "string": 
+					case 'string': 
 					{
 						// Image by coping from browser (HTML)
-						if (item.type === "text/html") {		
+						if (config.get('pasteImageHTML') && item.type === 'text/html') {		
 							item.getAsString(function (html) {
-								if (~(html.indexOf("<img"))) {
+								if (~(html.indexOf('<img'))) {
 									// Get vaild URL from HTML
-									html = html.substring(html.indexOf("src=") + 5);
-									var url = html.substring(0, html.indexOf("\""));
+									html = html.substring(html.indexOf('src=') + 5);
+									var url = html.substring(0, html.indexOf('"'));
 									
 									// Update
 									updateImageByURL(context, url);
@@ -83,10 +95,10 @@
 						} else
 							
 						// Image by coping link (only if no other already pasted)
-						if (item.type === "text/plain") {
+						if (config.get('pasteImageLink') && item.type === 'text/plain') {
 							// Do not change by link, if there is already choosen
-							if (true) { // @todo . configurable
-								if (context.find(".files")[0]) {
+							if (config.get('doNotChangeIfLink')) {
+								if (context.find('.files')[0]) {
 									break;
 								}
 							}
@@ -103,19 +115,19 @@
 						break;
 					}
 					
-					case "file":
+					case 'file':
 					{
 						// Image by coping from file (Images)
-						if (item.type.match(/image.*/)) {
+						if (config.get('pasteImageFile') && item.type.match(/image.*/)) {
 							var file = item.getAsFile();
 							
 							// Open (and hide) add media form 
-							context.find(".openAddMediaOverlay").click();
-							context.find(".addMediaOverlay").css("display", "none");
-							context.find(".overlay").css("display", "none");
+							context.find('.openAddMediaOverlay').click();
+							context.find('.addMediaOverlay').css('display', 'none');
+							context.find('.overlay').css('display', 'none');
 							
 							// Hacking Wykop' MFuploader script encapsulation through jQuery private event data
-							$._data(context.find(".embedFile").find('input[type=file]')[0], "events")["change"][0].handler({
+							$._data(context.find('.embedFile').find('input[type=file]')[0], 'events')['change'][0].handler({
 								preventDefault: function () {},
 								target: {
 									files: [file]
@@ -134,12 +146,12 @@
         /* Hooks */
         {
             // Hook to paste event of mikroblog post form
-            $(".reply").bind("paste", betterPaste);
+            $('.reply').bind('paste', betterPaste);
             
             // Keep hooking to paste events of comments form
-            $(".btnReply").bind("click", function () {
+            $('.btnReply').bind('click', function () {
                 setTimeout(function () {
-                    $(".replyOn").find(".reply").bind("paste", betterPaste);
+                    $('.replyOn').find('.reply').bind('paste', betterPaste);
                 }, 777);
             });
         }
